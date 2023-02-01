79a7c8e1f0c33e4409afd23cbfae53db277beca4
yuma-kitamura <jiyuujinunite@gmail.com>
2023-02-01 16:58:08 +0900

    update layouts
    - update header
    - separate main/sub
    - add footer for copyright

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config'
+import image from '@astrojs/image'
 import svelte from '@astrojs/svelte'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [svelte()],
+    integrations: [
+        svelte(),
+        image({
+            serviceEntryPoint: '@astrojs/image/sharp',
+        }),
+    ],
 })
