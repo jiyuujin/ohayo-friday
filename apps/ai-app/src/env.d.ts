8ea46c1554d254575558f706660021f9228e3541
yuma-kitamura <jiyuujinunite@gmail.com>
2023-04-26 09:40:58 +0900

    fix

--- apps/ai-app/src/env.d.ts
+++ apps/ai-app/src/env.d.ts
@@ -1,4 +1,6 @@
 import '../.astro/types.d.ts'
 
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="../.astro/type.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="@astrojs/image/client" />
